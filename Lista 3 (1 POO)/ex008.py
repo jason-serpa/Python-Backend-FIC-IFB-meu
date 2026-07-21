dbf78bb755c2eb1c44ed14a21bdd14efecb40c7f
Alunocest <Alunocest@ifb.local>
2026-07-21 16:31:47 -0300

    Ex 008 POO1

--- Lista 3 (1 POO)/ex008.py
+++ Lista 3 (1 POO)/ex008.py
@@ -12,3 +12,38 @@ ataque mágico. Na classe principal, crie instâncias das três
 classes e invoque o método atacar() para demonstrar o uso
 de herança e polimorfismo em tempo de execução.
 '''
+
+class Personagem:
+    def __init__(self, nome, nivel):
+        self.nome = nome
+        self.nivel = nivel
+    def atacar(self):
+        print(f"{self.nome}: Arranhão!")
+
+class Mago(Personagem):
+    def __init__(self, nome, nivel, mana):
+        super().__init__(nome, nivel)
+        self.mana = mana
+    def atacar(self):
+        print(f"{self.nome}: Corte rápido!")
+
+class Guerreiro(Personagem):
+    def __init__(self, nome, nivel, forca):
+        super().__init__(nome, nivel)
+        self.forca = forca
+    def atacar(self):
+        print(f"{self.nome}: Bola de fogo!")
+
+
+rato = Personagem(nome="Rato", nivel=1)
+guerreiro = Guerreiro(nome="Espadachim", nivel=10, forca=100)
+mago = Mago(nome="Feiticeiro", nivel=10, mana=100)
+
+print(f"\n{rato.nome} ataca!")
+rato.atacar()
+
+print(f"\n{guerreiro.nome} ataca!")
+guerreiro.atacar()
+
+print(f"\n{mago.nome} ataca!")
+mago.atacar()
